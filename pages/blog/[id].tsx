32b2d364810ade81c358741cae48061747e05a71
coffmanjrp <coffmanjrp@gmail.com>
2021-09-16 16:05:40 -0700

    Fix author info and tags postions

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -30,48 +30,58 @@ const BlogPostPage: NextPage<Props> = ({ frontmatter, source }) => {
 
   return (
     <Layout>
-      <div className="flex flex-row mx-auto">
-        <main className="flex-1 w-full max-w-screen-md mx-auto py-24 md:pt-24">
-          <div className="flex flex-col w-full">
-            <h1 className="text-5xl font-bold mb-2">{title}</h1>
-            <div className="flex justify-between md:items-center flex-col md:flex-row mt-2 w-full mb-4">
-              <div className="flex items-center">
-                <span className="relative inline-flex items-center justify-center text-center  flex-shrink-0 gap-2">
-                  <Image
-                    src="/images/portrait.png"
-                    alt="author"
-                    width={24}
-                    height={24}
-                    className="w-full h-full object-cover rounded-full"
-                  />
-                  <p>{author},</p>
+      <main className="flex-1 w-full max-w-screen-md mx-auto py-24 md:pt-24">
+        <div className="flex flex-col w-full">
+          <h1 className="text-5xl font-bold mb-2">{title}</h1>
+          <div className="inline-flex my-2">
+            {tagArray &&
+              tagArray.map((tag, index) => (
+                <Link key={index} href={`/blog/${tag}`}>
+                  <a className="inline-block bg-gray-200 dark:bg-gray-500 px-1 rounded-md mr-1 text-sm leading-5 no-underline select-none text-gray-600 dark:text-gray-50">
+                    # {tag}
+                  </a>
+                </Link>
+              ))}
+          </div>
+          <div className="flex justify-between md:items-center flex-col md:flex-row mt-2 w-full mb-4">
+            <div className="flex items-center">
+              <div className="relative inline-flex items-center justify-center flex-shrink-0 gap-2">
+                <Image
+                  src="/images/portrait.png"
+                  alt="author"
+                  width={60}
+                  height={60}
+                  className="w-full h-full object-cover rounded-full"
+                />
+                <div className="text-sm">
                   <p>
-                    Published at {format(parseISO(published), 'MMMM dd, yyyy')}
+                    Written By <span className="font-bold">{author}</span>
+                  </p>
+                  <p>
+                    Published on{' '}
+                    <span className="font-bold">
+                      {format(parseISO(published), 'MMMM dd, yyyy')}
+                    </span>
                   </p>
                   {updated && (
                     <p>
-                      , Updated at {format(parseISO(updated), 'MMMM dd, yyyy')}
+                      Updated on{' '}
+                      <span className="font-bold">
+                        {format(parseISO(updated), 'MMMM dd, yyyy')}
+                      </span>
                     </p>
                   )}
-                  {tagArray &&
-                    tagArray.map((tag, index) => (
-                      <Link key={index} href={`/blog/${tag}`}>
-                        <a className="inline-block bg-gray-200 dark:bg-gray-500 px-1 rounded-md mr-1 text-sm leading-5 no-underline select-none text-gray-600 dark:text-gray-50">
-                          # {tag}
-                        </a>
-                      </Link>
-                    ))}
-                </span>
+                </div>
               </div>
             </div>
           </div>
-          <div className="markdown markdown-blue dark:markdown-dark max-w-none">
-            <ApplyAnchorLinks>
-              <MDXRemote {...source} components={MDXComponents} />
-            </ApplyAnchorLinks>
-          </div>
-        </main>
-      </div>
+        </div>
+        <div className="markdown markdown-blue dark:markdown-dark max-w-none">
+          <ApplyAnchorLinks>
+            <MDXRemote {...source} components={MDXComponents} />
+          </ApplyAnchorLinks>
+        </div>
+      </main>
     </Layout>
   );
 };
