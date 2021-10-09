6587d7cba410b330c2cf29980e2d2a9dc467927d
coffmanjrp <coffmanjrp@gmail.com>
2021-10-09 01:07:44 -0700

    Change blog posts and post api

--- pages/api/blog/[slug].ts
+++ pages/api/blog/[slug].ts
@@ -1,13 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { serialize } from 'next-mdx-remote/serialize';
-import remarkGfm from 'remark-gfm';
-import remarkUnwrapImages from 'remark-unwrap-images';
-// @ts-ignore
-import mdxPrism from 'mdx-prism';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import { STRAPI_ENDPOINT } from '@/config/index';
-import { generatePlaiceholder } from '@/lib/plaiceholder';
+import { getBlogPost } from '@/lib/api';
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,83 +7,7 @@ export default async function handler(
 ) {
   const { slug } = req.query;
 
-  const strapi = await fetch(`${STRAPI_ENDPOINT}/blogs?slug=${slug}`);
-  const strapiData = await strapi.json();
-  const strapiSource = strapiData.map(
-    (data: {
-      markdown: string;
-      title: string;
-      published: string;
-      updated: string;
-      user: {
-        username: string;
-        portrait: {
-          formats: {
-            thumbnail: {
-              url: string;
-            };
-          };
-        };
-      };
-      tags: string;
-      cover: { url: string };
-    }) => {
-      return {
-        content: data.markdown,
-        data: {
-          title: data.title,
-          published: data.published,
-          updated: data.updated,
-          author: {
-            name: data.user.username,
-            portrait: data.user.portrait.formats.thumbnail.url,
-          },
-          tags: data.tags,
-          cover: data.cover.url,
-        },
-      };
-    }
-  );
-  const { data, content } = strapiSource[0];
-
-  const mdxSource = await serialize(content, {
-    mdxOptions: {
-      remarkPlugins: [remarkGfm, remarkUnwrapImages],
-      rehypePlugins: [
-        mdxPrism,
-        rehypeSlug,
-        [
-          rehypeAutolinkHeadings,
-          {
-            behavior: 'wrap',
-            properties: { className: ['anchor-tag'] },
-          },
-        ],
-      ],
-    },
-  });
-
-  const cover = await generatePlaiceholder(
-    data.cover,
-    '/images/placeholder.jpg'
-  );
-
-  const portrait = await generatePlaiceholder(
-    data.author.portrait,
-    '/images/portrait.png'
-  );
-
-  const post = {
-    source: mdxSource,
-    frontmatter: {
-      slug,
-      ...data,
-    },
-    plaiceholders: {
-      cover: { ...cover.img, blurDataURL: cover.base64 },
-      portrait: { ...portrait.img, blurDataURL: portrait.base64 },
-    },
-  };
+  const post = await getBlogPost(slug);
 
   res.status(200).json({ post });
 }
--- pages/api/blog/posts.ts
+++ pages/api/blog/posts.ts
@@ -1,30 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-// @ts-ignore
-import qs from 'qs';
-import { STRAPI_ENDPOINT } from '@/config/index';
-import { generatePlaiceholder } from '@/lib/plaiceholder';
-
-type StrapiSource = {
-  title: string;
-  published: string;
-  updated: string;
-  user: {
-    username: string;
-    portrait: {
-      url: string;
-    };
-  };
-  tags: string;
-  cover: { formats: { small: { url: string } } };
-  slug: string;
-};
-
-type SortPosts = {
-  frontmatter: {
-    published: number;
-    updated: number;
-  };
-};
+import { getBlogPostsList } from '@/lib/api';
 
 export default async function handler(
   req: NextApiRequest,
@@ -32,50 +7,7 @@ export default async function handler(
 ) {
   const { term } = req.query;
 
-  const q = qs.stringify({
-    _where: { _or: [{ tags_contains: term }] },
-  });
-
-  const strapi = await fetch(`${STRAPI_ENDPOINT}/blogs?${q}`);
-  const strapiData = await strapi.json();
-
-  const strapiSource = strapiData.map((data: StrapiSource) => {
-    return {
-      frontmatter: {
-        title: data.title,
-        published: data.published,
-        updated: data.updated,
-        author: {
-          name: data.user.username,
-          portrait: data.user.portrait.url,
-        },
-        tags: data.tags,
-        cover: data.cover.formats.small.url,
-        slug: data.slug,
-      },
-    };
-  });
-
-  const postsWithPlaiceholder = await Promise.all(
-    strapiSource.map(async (post: { frontmatter: { cover: string } }) => {
-      const { img, base64 } = await generatePlaiceholder(
-        post.frontmatter.cover,
-        '/images/placeholder.jpg'
-      );
-
-      return {
-        ...post,
-        plaiceholder: { img: { ...img, blurDataURL: base64 } },
-      };
-    })
-  );
-
-  const posts = postsWithPlaiceholder.sort((a, b) =>
-    Number(new Date((a as SortPosts).frontmatter.published)) <
-    Number(new Date((b as SortPosts).frontmatter.updated))
-      ? 1
-      : -1
-  );
+  const posts = await getBlogPostsList(term);
 
   res.status(200).json({ posts });
 }
